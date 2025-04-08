a2b357b6babcb973fdbd63d9bda8dbec3eb1f410
Swarnil Kokulwar <swarnil.sk@gmail.com>
2025-04-08 23:23:24 +0530

    some changes

--- src/Airdrop.jsx
+++ src/Airdrop.jsx
@@ -1,23 +1,46 @@
-import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-import React from 'react'
+// import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+// import React from 'react'
 
-function Airdrop() {
+// function Airdrop() {
+//     const wallet = useWallet();
+//     const { connection } = useConnection();
+
+
+//     async function SendAirdrop (){
+//         const amount = document.getElementById("publicKey").value
+//         await connection.requestAirdrop(wallet.publicKey, amount * 1000000000)
+//         alert("Airdroped")
+//     }
+   
+//   return (
+//     <div>
+//         <input id='publicKey' type="text" placeholder='Amount' />
+//         <button onClick={SendAirdrop}>Request Airdrop</button>
+//     </div>
+//   )
+// }
+
+// export default Airdrop
+
+
+
+import { useWallet } from "@solana/wallet-adapter-react";
+import { useConnection } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+export function Airdrop() {
     const wallet = useWallet();
     const { connection } = useConnection();
 
-
-    async function SendAirdrop (){
-        const amount = document.getElementById("publicKey").value
-        await connection.requestAirdrop(wallet.publicKey, amount * 1000000000)
-        alert("Airdroped")
+    async function Airdrop() {
+        let amount = document.getElementById("amount").value;
+        await connection.Airdrop(wallet.publicKey, amount * LAMPORTS_PER_SOL);
+        alert("Airdropped " + amount + " SOL to " + wallet.publicKey.toBase58());
     }
-   
-  return (
-    <div>
-        <input id='publicKey' type="text" placeholder='Amount' />
-        <button onClick={SendAirdrop}>Request Airdrop</button>
-    </div>
-  )
-}
 
-export default Airdrop
+    return <div>
+        <br/><br/>
+        <input id="amount" type="text" placeholder="Amount" />
+        <button onClick={Airdrop}>Request Airdrop</button>
+    </div>
+}
