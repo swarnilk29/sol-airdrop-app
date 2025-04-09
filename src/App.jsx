e305922fbf9491f7a52dda936be78292f320067e
Swarnil Kokulwar <swarnil.sk@gmail.com>
2025-04-09 23:00:51 +0530

    solve some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import React, { FC, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
-  WalletConnectButton,
     WalletModalProvider,
     WalletDisconnectButton,
     WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
-import Airdrop  from './Airdrop';
-// import { clusterApiUrl } from '@solana/web3.js'; // beacause we hardcoded the endpoint
+import { clusterApiUrl } from '@solana/web3.js';
 
-// Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
+import { SendTokens } from './SendTokens';
+import { SignMessage } from './SignMessage';
+import { Airdrop } from './Airdrop'; 
+import { ShowBalance } from './ShowBalance'; 
 
- 
+
 function App() {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   return (
-    <ConnectionProvider endpoint={"your rpc link"}>
-            <WalletProvider wallets={[]} autoConnect>
-                <WalletModalProvider>
-                    <WalletMultiButton />
-                    <WalletDisconnectButton />
-                    <div>
-                      hii
-                    </div>
-                    <Airdrop></Airdrop>
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-  )
+      <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={[]} autoConnect>
+              <WalletModalProvider>
+                <div style={{ display: 'flex', justifyContent: "space-between" }}>
+                  <WalletMultiButton />
+                  <WalletDisconnectButton />
+                </div>
+                <Airdrop/>
+                <ShowBalance /> 
+                <SignMessage />
+                <SendTokens />
+              </WalletModalProvider>
+          </WalletProvider>
+      </ConnectionProvider>
+  );
 }
 
 export default App
--- src/ShowBalance.jsx
+++ src/ShowBalance.jsx
@@ -1,7 +1,7 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
-export function ShowSolBalance() {
+export function ShowBalance() {
     const { connection } = useConnection();
     const wallet = useWallet();
 
