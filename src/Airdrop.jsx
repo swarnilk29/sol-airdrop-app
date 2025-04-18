1eb8d147f07f91893aa07f61c1a9b342f5ca360c
Swarnil Kokulwar <swarnil.sk@gmail.com>
2025-04-18 23:48:31 +0530

    .

--- src/Airdrop.jsx
+++ src/Airdrop.jsx
@@ -1,29 +1,3 @@
-// import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-// import React from 'react'
-
-// function Airdrop() {
-//     const wallet = useWallet();
-//     const { connection } = useConnection();
-
-
-//     async function SendAirdrop (){
-//         const amount = document.getElementById("publicKey").value
-//         await connection.requestAirdrop(wallet.publicKey, amount * 1000000000)
-//         alert("Airdroped")
-//     }
-   
-//   return (
-//     <div>
-//         <input id='publicKey' type="text" placeholder='Amount' />
-//         <button onClick={SendAirdrop}>Request Airdrop</button>
-//     </div>
-//   )
-// }
-
-// export default Airdrop
-
-
-
 import { useWallet } from "@solana/wallet-adapter-react";
 import { useConnection } from "@solana/wallet-adapter-react";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
